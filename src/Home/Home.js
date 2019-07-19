2b7225d75aae4aab96afacbcf010f66a229f827b
AkhileshNS <nsakhilesh02@gmail.com>
2019-07-19 11:26:34 +0530

    Added AceEditor

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,10 @@
 // External Modules
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/json';
+import 'brace/theme/github';
 
 // Local Styles
 import { HomeContainer } from 'Home/Home.styles';
@@ -8,15 +12,37 @@ import { HomeContainer } from 'Home/Home.styles';
 // Global Functions
 import { derive } from 'global/functions';
 
-const Home = props => {
+const Home = ({
+  code = "",
+  setCode = newValue => console.log(newValue)
+}) => {
   return (
     <HomeContainer>
-      <p>Home Screen</p>
+      {/* <AceEditor
+        value={code}
+        mode="json"
+        theme="github"
+        onChange={setCode}
+        name="ace"
+        editorProps={{$blockScrolling: true}}
+        setOptions={{
+          enableBasicAutocompletion: false,
+          enableLiveAutocompletion: false,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+        showPrintMargin={false}
+        style={{width: "100%"}}
+      /> */}
     </HomeContainer>
   );
 };
 
-const mapStoresToProps = derive({});
+const mapStoresToProps = derive({
+  code: "homeStore",
+  setCode: "homeStore"
+});
 
 export { Home };
 export default inject(mapStoresToProps)(observer(Home));
--- src/Home/Home.store.js
+++ src/Home/Home.store.js
@@ -1,11 +1,15 @@
-import { decorate } from "mobx";
+import { decorate, observable, action } from "mobx";
 
 class HomeStore {
+  code = ""
 
+  setCode = newCode => this.code = newCode;
 }
 
 decorate(HomeStore, {
+  code: observable,
 
+  setCode: action
 });
 
 export default new HomeStore();
